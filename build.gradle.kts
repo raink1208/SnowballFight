863eab853904a588d4086baba552b7a573ddd824
raink1208 <58714901+raink1208@users.noreply.github.com>
2022-09-28 01:08:24 +0900

    init commit

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    kotlin("jvm") version "1.7.0"
+    java
+
+    id("net.minecrell.plugin-yml.bukkit") version "0.5.1"
+}
+
+group = "com.github.raink1208"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    compileOnly("io.papermc.paper:paper-api:1.19.2-R0.1-SNAPSHOT")
+}
+
+bukkit {
+    main = "com.github.raink1208.snowballfight.Main"
+    apiVersion = "1.19.2"
+}
+
+tasks.jar {
+    archiveFileName.set("${project.name}.jar")
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+}
---
+++ src/main/kotlin/com/github/raink1208/snowballfight/Main.kt
@@ -0,0 +1,6 @@
+package com.github.raink1208.snowballfight
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Main: JavaPlugin() {
+}
