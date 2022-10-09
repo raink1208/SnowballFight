b741bc6ca558ba485d8d965ae02af580cf1f599d
raink1208 <58714901+raink1208@users.noreply.github.com>
2022-10-09 19:14:25 +0900

    コマンドを実装

--- src/main/kotlin/com/github/raink1208/snowballfight/commands/SnowballFightCommand.kt
+++ src/main/kotlin/com/github/raink1208/snowballfight/commands/SnowballFightCommand.kt
@@ -1,5 +1,6 @@
 package com.github.raink1208.snowballfight.commands
 
+import com.github.raink1208.snowballfight.Main
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
@@ -8,8 +9,23 @@ import org.bukkit.util.StringUtil
 
 class SnowballFightCommand: CommandExecutor, TabCompleter {
     private val subCommands = mutableListOf("create", "destroy")
-    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>?): Boolean {
-
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+        if (args.isEmpty()) return false
+        when (args[0]) {
+            "create" -> {
+                val res = Main.instance.createGame()
+                if (res) {
+                    sender.sendMessage("ゲームを作成しました")
+                } else {
+                    sender.sendMessage("既にゲームが存在します")
+                }
+            }
+            "destroy" -> {
+                Main.instance.destroyGame()
+                sender.sendMessage("ゲームを削除しました")
+            }
+            else -> return false
+        }
         return true
     }
 
