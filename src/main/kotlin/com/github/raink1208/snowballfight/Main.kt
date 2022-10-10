a81788e04035f94cc5d315100c96659666beac4a
raink1208 <58714901+raink1208@users.noreply.github.com>
2022-10-10 23:21:15 +0900

    イベントをMainから削除

--- src/main/kotlin/com/github/raink1208/snowballfight/Main.kt
+++ src/main/kotlin/com/github/raink1208/snowballfight/Main.kt
@@ -2,13 +2,9 @@ package com.github.raink1208.snowballfight
 
 import com.github.raink1208.snowballfight.commands.SnowballFightCommand
 import com.github.raink1208.snowballfight.game.SnowballFightGame
-import org.bukkit.entity.Player
-import org.bukkit.event.EventHandler
-import org.bukkit.event.Listener
-import org.bukkit.event.entity.ProjectileHitEvent
 import org.bukkit.plugin.java.JavaPlugin
 
-class Main: JavaPlugin(), Listener {
+class Main: JavaPlugin() {
     companion object {
         lateinit var instance: Main
         private set
@@ -17,7 +13,7 @@ class Main: JavaPlugin(), Listener {
     var game: SnowballFightGame? = null; private set
 
     fun createGame(): Boolean {
-        if (game != null) {
+        if (game == null) {
             game = SnowballFightGame()
             return true
         }
@@ -33,15 +29,7 @@ class Main: JavaPlugin(), Listener {
         game = null
     }
 
-    @EventHandler
-    fun onHit(event: ProjectileHitEvent) {
-        val hit = event.hitEntity as? Player ?: return
-        hit.velocity = event.entity.velocity
-        println(event.entity.velocity)
-    }
-
     override fun onEnable() {
-        server.pluginManager.registerEvents(this, this)
         instance = this
 
         getCommand("snowballfight")?.run {
