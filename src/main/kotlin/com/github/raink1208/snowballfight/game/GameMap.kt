7ce90b2e946380fccd53f3271340e289c403db53
raink1208 <58714901+raink1208@users.noreply.github.com>
2022-10-12 19:36:41 +0900

    ゲームのマップクラスを追加

---
+++ src/main/kotlin/com/github/raink1208/snowballfight/game/GameMap.kt
@@ -0,0 +1,7 @@
+package com.github.raink1208.snowballfight.game
+
+import org.bukkit.Location
+import org.bukkit.World
+
+class GameMap(val mapName: String, val world: World, val spawnLocations: List<Location>) {
+}
